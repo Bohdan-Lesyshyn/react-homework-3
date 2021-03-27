6f85963156f84556d2ab4fa7659832d8834483c6
Bohdan_Lesyshyn <amakhasla2@gmail.com>
2021-03-27 11:07:10 +0200

    homework3

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const blockImg = React.createElement(
+    "img", 
+    {
+        src: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fprogressive.ua%2Fwp-content%2Fuploads%2F2016%2F07%2F109.png&imgrefurl=https%3A%2F%2Fprogressive.ua%2Fwebsite_development%2F&tbnid=uCkDlA5tPHqIqM&vet=12ahUKEwjq2aiPgdDvAhUPwioKHVk3B0wQMygNegUIARDNAQ..i&docid=BFL1x3GKnW4ccM&w=396&h=263&q=%D1%81%D0%B0%D0%B9%D1%82&ved=2ahUKEwjq2aiPgdDvAhUPwioKHVk3B0wQMygNegUIARDNAQ",
+        alt: "sorry, img is loading"
+    })
+
+const blockH1 = React.createElement(
+    "h1",{},"text",
+)
+const blockDiv = React.createElement(
+    "div",
+    {
+        children: [blockImg, blockH1],
+    }
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(blockDiv, document.getElementById("root"));
